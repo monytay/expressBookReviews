8678aaed8ec399d492a76fcfff552d5f2e394838
monytay <t.majstorovic@outlook.com>
2024-11-08 06:17:01 -0500

    5th commit

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -64,8 +64,13 @@ public_users.get('/title/:title',function (req, res) {
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const isbn = parseInt(req.params.isbn);
+
+    if(books[isbn]) {
+        return res.status(200).json(books[isbn].reviews);
+    } else {
+        return res.status(404).send("No books found for the ISBN.");
+    }
 });
 
 module.exports.general = public_users;
