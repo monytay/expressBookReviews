6fe9c6c43f0e3080942f294fb562db594a389383
monytay <t.majstorovic@outlook.com>
2024-12-17 01:27:51 -0500

    7th task

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -9,15 +9,28 @@ const isValid = (username)=>{
     return !users.includes(username);
 }
 
-const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
+const authenticatedUser = (username,password)=>{
+    const user = users.find(user_case => user_case.username === username && user_case.password === pasword);
+    user ? true:false;
 }
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
+  const {username,password} = req.body;
+
+  if(!username || !password){
+    res.status(400).json({Message:"Username and password required"})
+  };
+  if (authenticatedUser(username,password)){
+    const token =jwt.sign({ username }, SECRET_KEY, {expiresIn:"1Hr"});
+
+    return res.status(200).json({
+        message:"Login successful",
+        token: token
+    });
+  } else {
+    return res.status(401).json({message: "Invalid username and/or password"})
+  }
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
