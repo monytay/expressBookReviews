a5cc693a202fcf171acc5660552254258a2e38b8
monytay <t.majstorovic@outlook.com>
2024-12-18 05:35:18 -0500

    Issues with task 7

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -3,34 +3,39 @@ const jwt = require('jsonwebtoken');
 let books = require("./booksdb.js");
 const regd_users = express.Router();
 
-let users = [];
+let users = [{
+    username:"Antonio",
+    password:"Hello123"
+}];
 
 const isValid = (username)=>{
     return !users.includes(username);
 }
 
 const authenticatedUser = (username,password)=>{
-    const user = users.find(user_case => user_case.username === username && user_case.password === pasword);
+    const user = users.find(user_case => user_case.username === username && user_case.password === password);
     user ? true:false;
 }
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
-  const {username,password} = req.body;
+    const {username,password} = req.body;
 
-  if(!username || !password){
-    res.status(400).json({Message:"Username and password required"})
-  };
-  if (authenticatedUser(username,password)){
-    const token =jwt.sign({ username }, SECRET_KEY, {expiresIn:"1Hr"});
+    if(!username || !password){
+        res.status(400).json({Message:"Username and password required"})
+    };
 
-    return res.status(200).json({
-        message:"Login successful",
-        token: token
-    });
-  } else {
-    return res.status(401).json({message: "Invalid username and/or password"})
-  }
+    if (authenticatedUser(username,password)){
+        const token =jwt.sign({ username }, SECRET_KEY, {expiresIn:"1Hr"});
+
+        return res.status(200).json({
+            message:"Login successful",
+            token: token
+        });
+    } else {
+        return res.status(401).json({message: "Invalid username and/or password"})
+    }
+});
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
